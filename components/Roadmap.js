ae79163debfe690f9b1bbc711a8195744d32de3b
epic-unicorn <struijs@gmail.com>
2022-04-06 21:56:28 +0200

    Replace class with className

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 export default function Roadmap() {
   return (
-    <div class="flex min-h-screen items-center bg-amber-100 ">
-      <div class="w-2/6 min-w-min pl-20">
-        <div class="flex flex-wrap">
-          <span class="font-amatic text-8xl text-black">
+    <div className="flex min-h-screen items-center bg-amber-100 ">
+      <div className="w-2/6 min-w-min pl-20">
+        <div className="flex flex-wrap">
+          <span className="font-amatic text-8xl text-black">
             Discover adventure is every trait
           </span>
         </div>
       </div>
-      <ul class="flex touch-auto space-x-10 p-20">
+      <ul className="flex touch-auto space-x-10 p-20">
         <li>
-          <div class="item h-80 w-80 -rotate-2 rounded-2xl bg-cover"></div>
+          <div className="item h-80 w-80 -rotate-2 rounded-2xl bg-cover"></div>
         </li>
         <li>
-          <div class="item h-80 w-80 rotate-3 rounded-2xl bg-cover"></div>
+          <div className="item h-80 w-80 rotate-3 rounded-2xl bg-cover"></div>
         </li>
       </ul>
     </div>
