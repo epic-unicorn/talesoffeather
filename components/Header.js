eb2aa49876b9646003aee68c6d43521074a5afd1
epic-unicorn <struijs@gmail.com>
2022-04-14 14:52:10 +0200

    class > classname

--- components/Header.js
+++ components/Header.js
@@ -71,14 +71,7 @@ export default function Header() {
             </ul>
           </nav>
         </div>
-      </header>
-      <div class="grid grid-flow-row-dense h-screen justify-center grid-cols-3 grid-rows-3">
-        <div class="col-span-2">01</div>
-        <div class="col-span-2">02</div>
-        <div>03</div>
-        <div>04</div>
-        <div>05</div>
-      </div>
+      </header>    
     </div>
   );
 }
