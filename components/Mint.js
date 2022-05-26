7cc96b9482c1d45d4eaa3bdc77550914b553674d
epic-unicorn <struijs@gmail.com>
2022-05-26 14:49:38 +0200

    env test

--- components/Mint.js
+++ components/Mint.js
@@ -30,7 +30,7 @@ const Mint = () => {
 
   useEffect(async () => {
     console.log("Node environment: " + process.env.NODE_ENV);
-    console.log("Vercel environment: " + process.env.VERCEL_ENV);
+    console.log("Vercel environment: " + VERCEL_ENV);
 
     checkIfWalletIsConnected();
     checkCorrectNetwork();
