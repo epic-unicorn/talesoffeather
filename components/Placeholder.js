6c08cb6a07780dab30fa80126d8c2bf7317cfcf4
epic-unicorn <struijs@gmail.com>
2022-05-31 22:54:55 +0200

    Add new favicon setup and meta info

---
+++ components/Placeholder.js
@@ -0,0 +1,5 @@
+export default function Placeholder() {
+    return (
+        <div className="flex col h-screen items-center justify-center">This is just a placeholder</div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Main from "../components/Main";
 import Mint from "../components/Mint";
-import Head from "next/head";
+import Meta from "../components/Meta";
+import Placeholder from "../components/Placeholder";
 
 export default function Home() {
   return (
     <div className="bg-th-background">
-      <Head>
-        <title>Tales of Feather</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-      </Head>
+      <Meta />
+      {/* <Placeholder /> */}
       {/* <Main /> */}
       <Mint />
     </div>
