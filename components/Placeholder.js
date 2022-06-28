ab80564b0dd82db1d8d6a1b3374c13b17801f515
epic-unicorn <struijs@gmail.com>
2022-06-28 23:26:05 +0200

    added logging

--- components/Placeholder.js
+++ components/Placeholder.js
@@ -1,5 +1,5 @@
 export default function Placeholder() {
-  return (
+  return (  
     <div className="flex col h-screen items-center justify-center">
       <h1 className="text-8xl font-black font-amatic">The story is not started yet...</h1>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import Main from "../components/Main";
 import Mint from "../components/Mint";
 import Meta from "../components/Meta";
 import Placeholder from "../components/Placeholder";
+import projectConfig from "../config/projectConfig";
 
 export default function Home() {
+
+  // print app info
+  console.log(`Sitedomain: ${projectConfig.siteDomain}`);
+  console.log(`Environment: ${process.env.NEXT_PUBLIC_VERCEL_ENV}`);
+  console.log(`Networkname: ${projectConfig.networkName}`);
+  console.log(`Mint cost: ${projectConfig.mintCost}`);
+  console.log(`OpenSea URL: ${projectConfig.openseaCollectionUrl}`);
+  console.log(`Contract address: ${projectConfig.contractAddress}`);
+  console.log(`EtherScan URL: ${projectConfig.etherScanUrl}`);
+
   return (
     <div className="bg-th-background">
       <Meta />
